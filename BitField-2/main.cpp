57675ab7839a6daa987853648f098312ac89f215
Bl4ckk3ys <ilchechnev@gmail.com>
2024-12-04 11:29:56 +0300

    asd

--- BitField-2/main.cpp
+++ BitField-2/main.cpp
@@ -4,21 +4,21 @@
 #include "BitField.h"
 
 int main(int argc, char **argv) {
-    // ::testing::InitGoogleTest(&argc, argv);
+    ::testing::InitGoogleTest(&argc, argv);
 
-    // return RUN_ALL_TESTS();
+    return RUN_ALL_TESTS();
 
 
-    BitField s1(10);
+    // BitField s1(10);
     
-    s1.SetBit(3);
-    s1.SetBit(2);
-    s1.SetBit(5);
-    s1.SetBit(9);
-    s1.SetBit(7);
-    s1.Print();
+    // s1.SetBit(3);
+    // s1.SetBit(2);
+    // s1.SetBit(5);
+    // s1.SetBit(9);
+    // s1.SetBit(7);
+    // s1.Print();
     
-    BitField s2 = s1<<2;
-    s2.Print();
+    // BitField s2 = s1<<2;
+    // s2.Print();
     return 0;
 }
