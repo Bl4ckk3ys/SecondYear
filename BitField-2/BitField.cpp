847fbf60cf53d24f5127ce1ba4d924ba5545090e
Bl4ckk3ys <ilchechnev@gmail.com>
2024-12-04 13:00:31 +0300

    sdsdfsdf

--- BitField-2/BitField.cpp
+++ BitField-2/BitField.cpp
@@ -85,22 +85,22 @@ BitField BitField::operator~(){
     }
     return copy;
 }
-BitField BitField::operator>>(size_t n){
-    BitField copy = BitField(_memSize);
+BitField BitField::operator<<(size_t n){
+    BitField copy = BitField(_sizeBit);
     for(size_t i = 0; i < _sizeBit-n; i++){
         if(GetBit(i+n)) copy.SetBit(i);
     }
     return copy;
 };
-BitField BitField::operator<<(size_t n){
-    BitField copy = BitField(_memSize);
-    for(size_t i = _memSize-n; i == 0; i--){
-        if(GetBit(i-n)) copy.SetBit(i);
+BitField BitField::operator>>(size_t n){
+    BitField copy = BitField(_sizeBit);
+    for(size_t i = 0; i < _sizeBit - n; i++){
+        if(GetBit(i)) copy.SetBit(i + n);
 }
     return copy;
 };
 void BitField::Print(){
-    for(size_t i = 0; i < 10; i++)
-        std::cout << _mem[GetMemIndex(i)] << " ";
+    for(size_t i = 0; i < _sizeBit; i++)
+        std::cout << +GetBit(i) << " ";
     std::cout<<"\n";    
 }
--- BitField-2/main.cpp
+++ BitField-2/main.cpp
@@ -4,21 +4,24 @@
 #include "BitField.h"
 
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
+    // ::testing::InitGoogleTest(&argc, argv);
 
-    return RUN_ALL_TESTS();
+    // return RUN_ALL_TESTS();
 
 
-    // BitField s1(10);
+    BitField s1(10);
     
-    // s1.SetBit(3);
-    // s1.SetBit(2);
-    // s1.SetBit(5);
-    // s1.SetBit(9);
-    // s1.SetBit(7);
-    // s1.Print();
-    
-    // BitField s2 = s1<<2;
-    // s2.Print();
+    s1.SetBit(3);
+    s1.SetBit(2);
+    s1.SetBit(5);
+    s1.SetBit(9);
+    s1.SetBit(7);
+    s1.Print();
+    BitField s2(10);
+    s2 = s1>>2;
+    s2.Print();
+    BitField s3(10);
+    s3 = s1<<2;
+    s3.Print();
     return 0;
 }
